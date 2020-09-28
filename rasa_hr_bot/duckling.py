adc0bf52bc70120614b17204663295560f40b70e
merveenoyan <merveenoyan@gmail.com>
2020-09-28 20:23:28 +0300

    initial commit

---
+++ rasa_hr_bot/duckling.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("docker run -p 8000:8000 rasa/duckling")
---
+++ rasa_hr_bot/setup.py
@@ -0,0 +1,5 @@
+import os 
+
+os.system("pip install -r requirements.txt")
+os.system("python3 -m spacy download en_core_web_md")
+os.system("python3 -m spacy download en_core_web_md en")
